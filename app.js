9827529c2cbaa1d17cd22b7cb0f8530e3d132376
SandlerJr <sandlerjunin@gmail.com>
2023-10-18 09:56:04 -0300

    Criamos uma função que recebe 2 parâmetros para gerar um número aleatório, Utilizamos um return dentro da função e criamos outra função para exibir mensagem na tela

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'jogo do número secreto';
+let numeroSecreto = gerarNumeroAleatorio();
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute(){
-    console.log(`Foi dado o clique no botão`)
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
 }
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
+}
+
